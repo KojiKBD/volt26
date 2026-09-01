baaf1c491649bd3e950b7969c90c610534947252
kojikbd-admin <monsteeh@gmail.com>
2026-09-01 16:15:37 +0200

    feat: redesign game over screen

---
+++ BGAnimations/ScreenGameOver background.lua
@@ -0,0 +1,23 @@
+return Def.ActorFrame{
+	Def.Sprite{
+		Name="GameOverBackground",
+		Texture=THEME:GetPathG("", "VOLT26/bg_ss@2x.png"),
+		InitCommand=function(self)
+			self:Center():scaletoclipped(_screen.w, _screen.h)
+		end,
+	},
+	Def.Quad{
+		Name="TopShade",
+		InitCommand=function(self)
+			self:align(0, 0):xy(0, 0):zoomto(_screen.w, _screen.h * 0.22)
+				:diffuse(color("#090909")):diffusealpha(0.44)
+		end,
+	},
+	Def.Quad{
+		Name="BottomShade",
+		InitCommand=function(self)
+			self:align(0, 1):xy(0, _screen.h):zoomto(_screen.w, _screen.h * 0.18)
+				:diffuse(color("#090909")):diffusealpha(0.38)
+		end,
+	},
+}
